40a19050a59e6a23afe2dc89fe45edf3dd56eca6
ChanMo <chen.orange@aliyun.com>
2016-11-17 12:57:16 +0800

    use future

--- wechat_message/models.py
+++ wechat_message/models.py
@@ -1,6 +1,9 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
+@python_2_unicode_compatible
 class Message(models.Model):
     keyword = models.CharField(_('keyword'), max_length=200, unique=True)
     content = models.TextField(_('content'))
@@ -8,9 +11,6 @@ class Message(models.Model):
     updated = models.DateTimeField(_('updated'), auto_now=True)
 
     def __str__(self):
-        return self.keyword
-
-    def __unicode__(self):
         return self.keyword
 
     class Meta(object):
